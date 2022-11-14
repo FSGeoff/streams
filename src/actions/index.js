fb841a9a77a45fddb77dfc212c83edf96754be9f
GeoffNixCodes <fullstackgeoff@gmail.com>
2022-11-14 10:26:19 -0500

    added edit feature

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from '../apis/streams';
+import history from '../history';
 import {
     SIGN_IN,
     SIGN_OUT,
@@ -27,6 +28,7 @@ export const createStream = formValues => async (dispatch, getState) => {
     const response = await streams.post('/streams', { ...formValues, userId });
 
     dispatch({ type: CREATE_STREAM, payload: response.data });
+    history.push('/');
 };
 
 export const fetchStreams = () => async dispatch => {
@@ -35,7 +37,7 @@ export const fetchStreams = () => async dispatch => {
     dispatch({ type: FETCH_STREAMS, payload: response.data });
 };
 
-export const fetchStream = (id) => async dispatch => {
+export const fetchStream = id => async dispatch => {
     const response = await streams.get(`/streams/${id}`);
 
     dispatch({ type: FETCH_STREAM, payload: response.data });
@@ -47,7 +49,7 @@ export const editStream = (id, formValues) => async dispatch => {
     dispatch({ type: EDIT_STREAM, payload: response.data });
 };
 
-export const deleteStream = (id) => async dispatch => {
+export const deleteStream = id => async dispatch => {
     await streams.delete(`/streams/${id}`);
 
     dispatch({ type: DELETE_STREAM, payload: id });
