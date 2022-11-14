00fd59ffa5365c63a5a94983fd4e3863da952b9a
GeoffNixCodes <fullstackgeoff@gmail.com>
2022-11-14 13:33:16 -0500

    issues getting initial values

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,6 +1,8 @@
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchStream } from '../../actions';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
 
 class StreamEdit extends React.Component {
   componentDidMount() {
@@ -8,21 +10,34 @@ class StreamEdit extends React.Component {
     this.props.fetchStream(this.props.match.params.id);
   }
 
+  onSubmit = (formValues) => {
+    console.log(formValues);
+  };
+
   render() {
     // eslint-disable-next-line react/prop-types
     if (!this.props.stream) {
       return <div>Loading...</div>;
     }
-    // eslint-disable-next-line react/prop-types
-    return <div>{this.props.stream.title}</div>;
+
+    return (
+        <div>
+          <h3>Edit a Stream</h3>
+          <StreamForm
+              //eslint-disable-next-line react/prop-types
+              initialValues={_.pick(this.props.stream, 'title', 'description')}
+              onSubmit={this.onSubmit}
+          />
+        </div>
+    );
   }
 }
 
 const mapStateToProps = (state, ownProps) => {
-  return { stream: state.streams[ownProps.match.params.id] };
+    return { stream: state.streams[ownProps.match.params.id] };
 };
 
 export default connect(
     mapStateToProps,
-    { fetchStream }
+    { fetchStream, editStream }
 )(StreamEdit);
